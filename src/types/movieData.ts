be95b97b85811997062cf6690bf8298c4c3093b3
Anastasia <158266030+Anastasiya-Stavrova@users.noreply.github.com>
2025-01-01 18:58:13 +0700

    init

---
+++ src/types/movieData.ts
@@ -0,0 +1,22 @@
+interface MovieDataType {
+  id: string;
+  title: string;
+  thumbnail: {
+    trending?: {
+      small: string;
+      large: string;
+    };
+    regular: {
+      small: string;
+      medium: string;
+      large: string;
+    };
+  };
+  year: number;
+  category: string;
+  rating: string;
+  isBookmarked: boolean;
+  isTrending: boolean;
+}
+
+export type { MovieDataType };
